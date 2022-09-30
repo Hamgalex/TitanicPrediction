8fd6143fb00463e7b51a04e3c48421f92137a5cc
Héctor Alejandro Márquez González <hamgalex@hotmail.com>
2022-09-30 17:49:12 -0500

    Se mergearon test y train con el passengerid

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+from sklearn.tree import DecisionTreeClassifier
+
 
 gender_submission = pd.read_csv("datasets/gender_submission.csv",encoding='utf-8')
 test = pd.read_csv("datasets/test.csv",encoding='utf-8')
 train = pd.read_csv("datasets/train.csv",encoding='utf-8')
+
+test=pd.merge(test,gender_submission,on="PassengerId",how="left")
+train=pd.merge(train,gender_submission,on="PassengerId",how="left")
 
 st.dataframe(gender_submission)
 
