ad04586a1a772bcc9e600e4efef70a5d19bf17fc
Héctor Alejandro Márquez González <hamgalex@hotmail.com>
2022-09-30 18:12:15 -0500

    se arreglo el merge de test

--- app.py
+++ app.py
@@ -4,17 +4,21 @@ import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
 
-
+# leer los csv's
 gender_submission = pd.read_csv("datasets/gender_submission.csv",encoding='utf-8')
 test = pd.read_csv("datasets/test.csv",encoding='utf-8')
 train = pd.read_csv("datasets/train.csv",encoding='utf-8')
 
-test=pd.merge(test,gender_submission,on="PassengerId",how="left")
-train=pd.merge(train,gender_submission,on="PassengerId",how="left")
+# mergear los csv's
+test=pd.merge(test,gender_submission,on="PassengerId", how="left")
 
 st.dataframe(gender_submission)
-
 st.dataframe(train)
-
 st.dataframe(test)
+
+# contar cuantos sobrevivieron
+# num_sobrevivientes = train.Survived.value_counts().to_list()
+# st.write(num_sobrevivientes)
+
+
 
