86b9ce3faa7f4f8f146e7e91acf8507ca9be9693
Héctor Alejandro Márquez González <hamgalex@hotmail.com>
2022-10-14 11:31:37 -0500

    Se añadió gráfica de sobrevivientes por rango de edad

--- app.py
+++ app.py
@@ -26,7 +26,23 @@ num_sobrevivientes = test.Survived.value_counts().to_list()
 fig = plt.figure(figsize = (10, 5))
 plt.bar(["Female","Male"], num_sobrevivientes, color=['pink','lightskyblue'])
 plt.title("Survivors per Sex") 
-
 st.header("Number of survivors of the titanic per Sex")
 st.pyplot(fig)
 
+
+# Sobrevivientes
+sobrevivientes=train.loc[train.Survived==1]
+
+# ver las edades de los sobrevivientes
+bins=[0,10,18,30,50,100]
+labels=['Child','Teen','Young Adult','Adult','Elderly']
+categoria_edades=sobrevivientes
+categoria_edades['Age_Category']=pd.cut(categoria_edades['Age'],bins,labels=labels)
+categoria_edades_cont = categoria_edades.Age_Category.value_counts().to_list()
+
+# graficar
+fig = plt.figure(figsize = (10, 5))
+plt.bar(labels,categoria_edades_cont,color=['lightgray','silver','darkgrey','grey','dimgrey'])
+plt.title("Survivors per group") 
+st.header("Number of survivors per age group")
+st.pyplot(fig)
